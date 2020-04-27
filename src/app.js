45a29b42e58abcdfc8b799e2ca3ffca0fe1a9305
Lauren Thorud <52000191+LalaIAm@users.noreply.github.com>
2020-04-27 00:56:18 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// Personal API Key for OpenWeatherMap API
+
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '' + d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
