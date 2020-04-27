4329c5235c105220838c92a1f9604bc13cbee5b0
Lauren Thorud <52000191+LalaIAm@users.noreply.github.com>
2020-04-27 01:14:24 -0500

    feat: Setup express server

--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 // Setup empty JS object to act as endpoint for all routes
 // Express to run server and routes
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+const cors = require( 'cors' );
+const PORT = process.env.PORT || 4000;
+
+const projectData = {}
+
+//Helper Functions
+const addData = ( req, res ) => {
+    let data = req.body;
+    let newEntry = {
+        date: data.date,
+        input: data.input,
+        temp: data.temp
+    }
+
+    projectData.push( newEntry );
+    return newEntry;
+}
 
 // Start up an instance of app
+const app = express();
+
 
 /* Dependencies */
 /* Middleware*/
-
-//Here we are configuring express to use body-parser as middle-ware.
-// Cors for cross origin allowance
+app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( bodyParser.json() );
+app.use( cors() );
 
 // Initialize the main project folder
+app.use( express.static( 'src' ) );
 
 // Spin up the server
 // Callback to debug
+app.listen( PORT, () => {
+    console.log( `Listening at http://localhost:${ PORT }` );
+} )
 
 // Initialize all route with a callback function
 
 // Callback function to complete GET '/all'
+app.get( '/all', ( req, res ) => {
+    res.send( appData );
+})
 
 // Post Route
+app.post( '/add', addData );
   
